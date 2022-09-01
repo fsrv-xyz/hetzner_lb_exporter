7cccf9e9d240c66d60c5df908eb27248086e6b8d
Florian Bauer <florian@fsrv.xyz>
2022-09-01 18:56:00 +0200

    chore: initial commit

---
+++ cmd/hetzner_lb_exporter/main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"context"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/hetznercloud/hcloud-go/hcloud"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	labelLoadBalancerID     = "loadbalancer_id"
+	labelLoadBalancerName   = "loadbalancer_name"
+	labelEndpointListenPort = "endpoint_listen_port"
+	labelTargetIdentifier   = "target_identifier"
+	labelTrafficDirection   = "traffic_direction"
+)
+
+var (
+	metricLoadBalancerTargetCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "hetzner_lb",
+		Name:      "target_count",
+		Help:      "Number of targets in a load balancer",
+	}, []string{
+		labelLoadBalancerID,
+		labelLoadBalancerName,
+	})
+
+	metricLoadBalancerServiceCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "hetzner_lb",
+		Name:      "service_count",
+		Help:      "Number of services in a load balancer",
+	}, []string{
+		labelLoadBalancerID,
+		labelLoadBalancerName,
+	})
+
+	metricLoadBalancerTargetHealthStatus = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "hetzner_lb",
+		Name:      "target_health_status",
+		Help:      "Health status of a target",
+	}, []string{
+		labelLoadBalancerID,
+		labelEndpointListenPort,
+		labelTargetIdentifier,
+	})
+
+	metricLoadBalancerTraffic = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "hetzner_lb",
+		Name:      "traffic_bytes",
+		Help:      "traffic of a load balancer",
+	}, []string{
+		labelLoadBalancerID,
+		labelTrafficDirection,
+	})
+)
+
+type Parameters struct {
+	ApiKey           string
+	WebListenAddress string
+}
+
+var parameters Parameters
+
+func init() {
+	app := kingpin.New(os.Args[0], "Hetzner Load Balancer Exporter")
+	app.Flag("hetzner.api-key", "Hetzner API key").Envar("HETZNER_API_TOKEN").Required().StringVar(&parameters.ApiKey)
+	app.Flag("web.listen-address", "Address to listen on for web interface and telemetry.").Default(":9115").StringVar(&parameters.WebListenAddress)
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	prometheus.MustRegister(
+		metricLoadBalancerTargetCount,
+		metricLoadBalancerServiceCount,
+		metricLoadBalancerTargetHealthStatus,
+		metricLoadBalancerTraffic,
+
+	)
+}
+
+func refreshWorker(ctx context.Context, done chan any, client *hcloud.Client) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("refreshWorker: context done")
+			done <- true
+			return
+		case <-time.After(time.Second * 2):
+			loadbalancers, _, loadbalancerListError := client.LoadBalancer.List(context.Background(), hcloud.LoadBalancerListOpts{})
+			if loadbalancerListError != nil {
+				panic(loadbalancerListError)
+			}
+
+			for _, loadbalancer := range loadbalancers {
+				metricLoadBalancerTargetCount.With(prometheus.Labels{labelLoadBalancerID: strconv.Itoa(loadbalancer.ID), labelLoadBalancerName: loadbalancer.Name}).Set(float64(len(loadbalancer.Targets)))
+				metricLoadBalancerServiceCount.With(prometheus.Labels{labelLoadBalancerID: strconv.Itoa(loadbalancer.ID), labelLoadBalancerName: loadbalancer.Name}).Set(float64(len(loadbalancer.Services)))
+
+				for _, target := range loadbalancer.Targets {
+					for _, health := range target.HealthStatus {
+						var status int
+						switch health.Status {
+						case hcloud.LoadBalancerTargetHealthStatusStatusHealthy:
+							status = 1
+						case hcloud.LoadBalancerTargetHealthStatusStatusUnhealthy:
+							status = 2
+						case hcloud.LoadBalancerTargetHealthStatusStatusUnknown:
+							status = 3
+						}
+						metricLoadBalancerTargetHealthStatus.With(prometheus.Labels{labelLoadBalancerID: strconv.Itoa(loadbalancer.ID), labelEndpointListenPort: strconv.Itoa(health.ListenPort), labelTargetIdentifier: target.IP.IP}).Set(float64(status))
+					}
+				}
+
+				metricLoadBalancerTraffic.With(prometheus.Labels{labelLoadBalancerID: strconv.Itoa(loadbalancer.ID), labelTrafficDirection: "in"}).Set(float64(loadbalancer.IngoingTraffic))
+				metricLoadBalancerTraffic.With(prometheus.Labels{labelLoadBalancerID: strconv.Itoa(loadbalancer.ID), labelTrafficDirection: "out"}).Set(float64(loadbalancer.OutgoingTraffic))
+			}
+		}
+	}
+}
+
+func main() {
+	// capture input signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	client := hcloud.NewClient(hcloud.WithToken(parameters.ApiKey))
+
+	workerContext, workerCancel := context.WithCancel(context.Background())
+	workerDone := make(chan any)
+	go refreshWorker(workerContext, workerDone, client)
+
+	server := &http.Server{
+		Addr:    parameters.WebListenAddress,
+		Handler: nil,
+	}
+
+	// set http server routes
+	http.Handle("/", http.RedirectHandler("/metrics", http.StatusPermanentRedirect))
+	http.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// wait for incoming interrupts
+	<-sigChan
+
+	workerCancel()
+
+	// shutdown server
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+	<-workerDone
+}
---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/fsrv-xyz/hetzner_lb_exporter
+
+go 1.18
+
+require (
+	github.com/hetznercloud/hcloud-go v1.35.2
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1 // indirect
+)
